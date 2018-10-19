ad4fd1960d1802813d6c5eb8a2433a865429b5a9
toshikazu.ohashi <toshikazu.ohashi@iridge.jp>
2018-10-19 18:12:00 +0900

    Remove all code integration to main

--- main.go
+++ main.go
@@ -1,9 +1,105 @@
 package main
 
-func main() {
-	uicmd := &UICmd{UI: NewBasicUI()}
-	uicmd.RunPrint()
+import (
+	"fmt"
+	"io"
+	"os"
+)
 
-	commandcmd := &CommandCmd{UI: NewBasicUI()}
-	commandcmd.RunExec()
+// IssueCmd Issueの操作を実行するコマンド
+// インタフェースにて任意の振る舞いを注入する
+type IssueCmd struct {
+	// UI Stdin/Stdoutによるコマンド入力/出力するインタフェース
+	UI UI
+	// Client GitLabAPIにリクエストを送りIssueを操作するインタフェース
+	Client IssueClient
+}
+
+// RunGetIssue Issue取得を行う処理
+func (c *IssueCmd) RunGetIssue() {
+	// Issueの取得
+	issue, _ := c.Client.GetIssue("foo/bar", 12)
+
+	// Issueの情報をStdoutに出力
+	res := fmt.Sprintf("ID:%d, Title:%s, Description:%s", issue.ID, issue.Title, issue.Description)
+	c.UI.Println(res)
+}
+
+// RunCreateIssue Issue更新を行う処理
+func (c *IssueCmd) RunCreateIssue() {
+	// Issueの作成
+	issue, _ := c.Client.CreateIssue("foo/bar", "newtitle", "newdescription")
+
+	// 作成したIssueの情報を出力(IDのみ)
+	res := fmt.Sprintf("created issue ID:%d", issue.ID)
+	c.UI.Println(res)
+}
+
+// UI Stdin/Stdoutによるコマンド入力/出力するインタフェース
+type UI interface {
+	Println(string)
+}
+
+// BasicUI 単純なUI
+type BasicUI struct {
+	Out io.Writer
+}
+
+// NewBasicUI コンストラクタ
+func NewBasicUI() UI {
+	// 単純にStdoutで初期化
+	return &BasicUI{
+		Out: os.Stdout,
+	}
+}
+
+// Println fmt.Printlnのラッパー
+func (u *BasicUI) Println(val string) {
+	fmt.Fprint(u.Out, val+"\n")
+}
+
+// Issue Issueのデータを格納する構造体
+type Issue struct {
+	// ID ID
+	ID int
+	// Title タイトル
+	Title string
+	// Description タイトル
+	Description string
+}
+
+// IssueClient Issueの操作を行うインタフェース
+type IssueClient interface {
+	// GetIssue 指定されたIDのIssueを取得
+	GetIssue(repo string, id int) (*Issue, error)
+	// CreateIssue 指定されたIDのIssueを取得
+	CreateIssue(repo, title, description string) (*Issue, error)
+}
+
+// GitlabIssueClient GitlabのIssueを取得する構造体
+type GitlabIssueClient struct{}
+
+// NewGitlabIssueClient コンストラクタ
+func NewGitlabIssueClient() IssueClient {
+	return &GitlabIssueClient{}
+}
+
+// GetIssue 割愛(実際にリクエストするコードがあると思ってください)
+func (m *GitlabIssueClient) GetIssue(repo string, id int) (*Issue, error) {
+	return &Issue{}, nil
+}
+
+// CreateIssue 割愛(実際にリクエストするコードがあると思ってください)
+func (m *GitlabIssueClient) CreateIssue(repo, title, description string) (*Issue, error) {
+	return &Issue{}, nil
+}
+
+func main() {
+	// 実行するコマンドを作成
+	issueCmd := &IssueCmd{
+		UI:     NewBasicUI(),
+		Client: NewGitlabIssueClient(),
+	}
+	// Issue取得を実行
+	issueCmd.RunGetIssue()
 }
---
+++ main_test.go
@@ -0,0 +1,154 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"testing"
+)
+
+//=======================================================================
+// シンプルにMockするよ！！！
+//=======================================================================
+
+// MockUI UIの入出力をMockする構造体
+type MockUI struct {
+	Out *bytes.Buffer
+}
+
+// NewMockUI コンストラクタ
+func NewMockUI() *MockUI {
+	return &MockUI{
+		Out: new(bytes.Buffer),
+	}
+}
+
+// Println fmt.Printlnのラッパー
+func (u *MockUI) Println(val string) {
+	fmt.Fprint(u.Out, val+"\n")
+}
+
+// MockIssueClientSimple シンプルにMockするための構造体
+type MockIssueClientSimple struct{}
+
+// GetIssue 単純に固定でIssueを返す
+func (m *MockIssueClientSimple) GetIssue(repo string, id int) (*Issue, error) {
+	return &Issue{
+		ID:          12,
+		Title:       "Title12",
+		Description: "Description12",
+	}, nil
+}
+
+// CreateIssue 割愛
+func (m *MockIssueClientSimple) CreateIssue(repo, title, description string) (*Issue, error) {
+	return nil, nil
+}
+
+// TestIssueCmd_RunGetIssue_Simple 単純なMockのテスト
+func TestIssueCmd_RunGetIssue_Simple(t *testing.T) {
+
+	ui := NewMockUI()
+	cmd := &IssueCmd{
+		// Stdoutではなくbytes.Bufferを渡すMockUIを使うことで後で出力文字列を取れる
+		UI: ui,
+		// 実際のIssueを取得するクライアントではなくNewMockUIを
+		Client: &MockIssueClientSimple{},
+	}
+
+	// 実行する
+	cmd.RunGetIssue()
+
+	// bytes.Bufferにしておけば気軽に出力文字列を取れる
+	got := ui.Out.String()
+	want := "ID:12, Title:Title12, Description:Description12\n"
+	if got != want {
+		t.Errorf("invalid output, \ngot: %#v\nwant:%#v", got, want)
+	}
+}
+
+//=======================================================================
+// いやいや固定値を返すだけじゃIssueの状態によって動作変更するパターン無理じゃん
+//
+// というわけで柔軟にMockするよ！！！
+//=======================================================================
+
+// MockIssueClientSpecific 柔軟にMockするための構造体
+type MockIssueClientSpecific struct {
+	// Mock用にInterfaceで定義した関数と同様の定義を持つ関数を持つのがミソ
+	MockGetIssue    func(repo string, id int) (*Issue, error)
+	MockCreateIssue func(repo, title, description string) (*Issue, error)
+}
+
+// GetIssue ここでは関数を定義せずにMock用関数をそのまま実行する
+func (m *MockIssueClientSpecific) GetIssue(repo string, id int) (*Issue, error) {
+	return m.MockGetIssue(repo, id)
+}
+
+// CreateIssue 同上
+func (m *MockIssueClientSpecific) CreateIssue(repo, title, description string) (*Issue, error) {
+	return m.MockCreateIssue(repo, title, description)
+}
+
+// TestIssueCmd_RunGetIssue_Specific テスト毎にGetIssueの動作を変更する柔軟なMockのテスト
+func TestIssueCmd_RunGetIssue_Specific(t *testing.T) {
+	ui := NewMockUI()
+	cmd := &IssueCmd{
+		UI: ui,
+		// Mockする関数に対して関数を定義して渡す
+		Client: &MockIssueClientSpecific{
+			MockGetIssue: func(repo string, id int) (*Issue, error) {
+				return &Issue{
+					ID:          12,
+					Title:       "Title12",
+					Description: "Description12",
+				}, nil
+			},
+		},
+	}
+
+	cmd.RunGetIssue()
+
+	got := ui.Out.String()
+	want := "ID:12, Title:Title12, Description:Description12\n"
+	if got != want {
+		t.Errorf("invalid output, \ngot: %#v\nwant:%#v", got, want)
+	}
+}
+
+//=======================================================================
+// いやいやちゃんと渡した値が想定どおりの値になっていることを保証出来なきゃ駄目でしょ
+//
+// というわけでかっちりMockするよ！！！
+//=======================================================================
+
+// TestIssueCmd_RunGetIssue_Specific テスト毎にCreateIssueの動作を変更し、値の検証まで行う
+func TestIssueCmd_RunCreateIssue(t *testing.T) {
+	ui := NewMockUI()
+	cmd := &IssueCmd{
+		UI: ui,
+		Client: &MockIssueClientSpecific{
+			MockCreateIssue: func(repo, title, description string) (*Issue, error) {
+				// testing.Tがこのスコープにおいても有効であるので
+				// 引数に対してそれぞれの値が想定どおりであることを確認する
+				wantTitle := "newtitle"
+				if title != wantTitle {
+					t.Errorf("invalid title \ngot: %s\nwant:%s", title, wantTitle)
+				}
+				wantDescription := "newdescription"
+				if description != wantDescription {
+					t.Errorf("invalid description \ngot: %s\nwant:%s", description, wantDescription)
+				}
+				return &Issue{
+					ID: 12,
+				}, nil
+			},
+		},
+	}
+	cmd.RunCreateIssue()
+
+	got := ui.Out.String()
+	want := "created issue ID:12\n"
+	if got != want {
+		t.Errorf("invalid output, \ngot: %#v\nwant:%#v", got, want)
+	}
+}
